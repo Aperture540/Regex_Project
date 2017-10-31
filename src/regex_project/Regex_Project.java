1ff717eeb18a3795c1ef3fda96fee376506cd5fb
Anthony Gendron <aperturescience540@gmail.com>
2017-10-31 13:01:12 -0400

    Regex_Project

--- src/regex_project/Regex_Project.java
+++ src/regex_project/Regex_Project.java
@@ -5,17 +5,47 @@
  */
 package regex_project;
 
+import java.util.Scanner;
+import java.util.regex.*;
+
 /**
  *
  * @author agendron
  */
 public class Regex_Project {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String phoneNumber;
+        Pattern numPat = Pattern.compile("([(]{0,})([0-9]{3})([)]{0,}[ ]{0,}[-]{0,}[.]{0,})([0-9]{3})([-]{0,}[.]{0,})([0-9]{4})");
+        
+        phoneNumber = getNumber();
+        if (!phoneNumber.equals("")) {
+            Matcher numMatch = numPat.matcher(phoneNumber);
+            
+            if(numMatch.matches()) {
+                String punct1 = numMatch.group(1);
+                String aCode = numMatch.group(2);
+                String punct2 = numMatch.group(3);
+                String cOCode = numMatch.group(4);
+                String punct3 = numMatch.group(5);
+                String sCode = numMatch.group(6);
+                System.out.println("(" + aCode + ")" + " " + cOCode + "-" + sCode);
+            }
+            else {
+                System.out.println("Error: Invalid format.");
+            }
+        }
+        else {
+            System.out.println("Error: No number.");
+        }
     }
-    
+
+    private static String getNumber() {
+        String phoneNumber;
+        Scanner numberInput = new Scanner(System.in);
+        System.out.println("Please enter a valid phone number with the area code.");
+        phoneNumber = numberInput.nextLine();
+        numberInput.close();
+        return phoneNumber;
+    }
 }
